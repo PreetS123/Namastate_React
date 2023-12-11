92a7019c9cdd6c3aba576eee216e78539c82a6d5
PreetS123 <j9183208@gmail.com>
2023-12-11 20:37:15 +0530

    function on onClick

--- react-interview/src/App.js
+++ react-interview/src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
+import {useState} from "react";
 import {Slider} from "./component/Slider";
 
 function App() {
+const [count,setCount]= useState(0);
+
+const getCont=()=>{
+  let a=1;
+  setCount(count+a);
+  return count;
+}
+function alertHi(){
+  alert("hi")
+}
 
   return (
     <div className="App">
-      <Slider/>
+      {/*<Slider/>*/}
+      {/** It will give the hi message when our browser gets rendered */}
+      <button onClick={alertHi()}>Click</button>
+      {/** It will give the hi message at click of button */}
+      <button onClick={alertHi}>Click</button>
+      {/** It will not give the hi message */}
+      <button onClick={()=>alertHi}>Click</button>
+     {/** It will give the hi message at click of button */}
+      <button onClick={()=>alertHi()}>Click</button>
     </div>
   );
 }
