ae4791eb6e67bb93f0f33ba9dd81d789b465fa01
PreetS123 <preetbhardwaj517@gmail.com>
2024-01-04 21:45:23 +0530

    function curring using bind method

---
+++ fronted-interview/curringFunction.js
@@ -0,0 +1,12 @@
+// Curring function--> It is basically a technique in javaScript , that transform the function of multiple argument into several function of a single argument.
+
+// Function curring using bind method
+
+let multiply= function (x,y){
+    console.log(x*y);
+}
+
+let multiplyByTwo=multiply.bind(this,2);
+multiplyByTwo(8);
+let multiplyByThree=multiply.bind(this,3,5); //x=3 and y=5 
+multiplyByThree(6); // third arg is being ignored
